13a5c0a7779da8749893ad3d45b994c83fd3f283
himitery <himit0131@naver.com>
2021-05-25 10:42:40 +0900

    c++: report #4

--- 2-1/cpp/report_4/3.cpp
+++ 2-1/cpp/report_4/3.cpp
@@ -3,53 +3,54 @@ using std::cout;
 using std::endl;
 
 template <class T> int find(T object, T *list, int size) {
-    for (int i = 0; i < size; i++)
-        if (list[i] == object)
-            return i;
-    return -1;
+  for (int i = 0; i < size; i++)
+    if (list[i] == object)
+      return i;
+  return -1;
 }
 
 int main(int argc, char *argv[]) {
-    int int_array[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20};
-    float float_array[] = {1.1, 2.2, 3.3, 4.4, 5.5, 6.6, 7.7, 8.8, 9.9, 10.01};
-    char char_array[] = {'h', 'i', 'm', 'i', 't', 'e', 'r', 'y'};
+  int int_array[] = {1,  2,  3,  4,  5,  6,  7,  8,  9,  10,
+                     11, 12, 13, 14, 15, 16, 17, 18, 19, 20};
+  float float_array[] = {1.1, 2.2, 3.3, 4.4, 5.5, 6.6, 7.7, 8.8, 9.9, 10.01};
+  char char_array[] = {'h', 'i', 'm', 'i', 't', 'e', 'r', 'y'};
 
-    cout << "int_array = {";
-    for (int i : int_array)
-        cout << i << ", ";
-    cout << "\b\b}" << endl << endl;
+  cout << "int_array = {";
+  for (int i : int_array)
+    cout << i << ", ";
+  cout << "\b\b}" << endl << endl;
 
-    cout << "find(10, int_array, sizeof(int_array))" << endl;
-    cout << "> " << find(10, int_array, sizeof(int_array)) << endl;
+  cout << "find(10, int_array, sizeof(int_array))" << endl;
+  cout << "> " << find(10, int_array, sizeof(int_array)) << endl;
 
-    cout << "find(30, int_array, sizeof(int_array))" << endl;
-    cout << find(30, int_array, sizeof(int_array)) << endl;
+  cout << "find(30, int_array, sizeof(int_array))" << endl;
+  cout << "> " << find(30, int_array, sizeof(int_array)) << endl;
 
-    cout << endl << endl;
+  cout << endl << endl;
 
-    cout << "float_array = {";
-    for (float i : float_array)
-        cout << i << ", ";
-    cout << "\b\b}" << endl << endl;
+  cout << "float_array = {";
+  for (float i : float_array)
+    cout << i << ", ";
+  cout << "\b\b}" << endl << endl;
 
-    cout << "find((float)3.3, float_array, sizeof(float_array))" << endl;
-    cout << "> " << find((float)3.3, float_array, sizeof(float_array)) << endl;
+  cout << "find((float)3.3, float_array, sizeof(float_array))" << endl;
+  cout << "> " << find((float)3.3, float_array, sizeof(float_array)) << endl;
 
-    cout << "find((float)10.2, float_array, sizeof(float_array))" << endl;
-    cout << find((float)10.2, float_array, sizeof(float_array)) << endl;
+  cout << "find((float)10.2, float_array, sizeof(float_array))" << endl;
+  cout << "> " << find((float)10.2, float_array, sizeof(float_array)) << endl;
 
-    cout << endl << endl;
+  cout << endl << endl;
 
-    cout << "char_array = {";
-    for (char i : char_array)
-        cout << i << ", ";
-    cout << "\b\b}" << endl << endl;
+  cout << "char_array = {";
+  for (char i : char_array)
+    cout << i << ", ";
+  cout << "\b\b}" << endl << endl;
 
-    cout << "find('y', char_array, sizeof(char_array))" << endl;
-    cout << find('y', char_array, sizeof(char_array)) << endl;
+  cout << "find('y', char_array, sizeof(char_array))" << endl;
+  cout << "> " << find('y', char_array, sizeof(char_array)) << endl;
 
-    cout << "find('a', char_array, sizeof(char_array))" << endl;
-    cout << "> " << find('a', char_array, sizeof(char_array)) << endl;
+  cout << "find('a', char_array, sizeof(char_array))" << endl;
+  cout << "> " << find('a', char_array, sizeof(char_array)) << endl;
 
-    return 0;
+  return 0;
 }
